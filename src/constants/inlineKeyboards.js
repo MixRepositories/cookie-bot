f9fd967366cdc112397fb5de3272154ef506d7d9
Alexander Cheprasov <a.cheprasov@aic.ru>
2021-04-02 10:56:11 +0700

    [CREATE]: constants for inline keyboards

---
+++ src/constants/inlineKeyboards.js
@@ -0,0 +1,15 @@
+const inlineKeyboards = {
+  callbacks: {
+    dislike: {
+      text: '👎', action: 'dislike'
+    },
+    like: {
+      text: '👍', action: 'like'
+    },
+    share: {
+      text: 'Поделиться  📣', action: 'share'
+    }
+  }
+}
+
+module.exports = inlineKeyboards
