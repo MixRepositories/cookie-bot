7dbec7968211e75918d6767df023129d82595037
Alexander Cheprasov <a.cheprasov@aic.ru>
2021-04-04 16:31:23 +0700

    [UPDATE]: add method for add prediction in database

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const db = require('./db')
 const bot = require('./src/bot')
-
+const getRowsWithPredictions = require('./src/utils/googleSheets/getRowsWithPredictions')
+const Prediction = require('./src/models/Prediction')
+const Language = require('./src/models/Language')
 db
   .then(() => {
     bot()
+    getRowsWithPredictions()
+      .then(async rows => {
+        const predictionObjects = []
+        const lang = await Language.findOne({ code: 'ru' })
+
+        rows.forEach(elem => {
+          predictionObjects.push({
+            text: elem.textPrediction,
+            language: lang._id
+          })
+        })
+
+        await Prediction.insertMany(
+          predictionObjects
+        )
+      })
   })
   .catch(error => {
     console.log('errorCatch', error)
