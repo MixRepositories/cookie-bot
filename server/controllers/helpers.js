8dbc4da718a1f31f62e4e81293e59f774474f5c1
Alexander Cheprasov <a.cheprasov@aic.ru>
2021-04-06 10:36:57 +0700

    [UPDATE]: add defense from conditions

--- server/controllers/helpers.js
+++ server/controllers/helpers.js
@@ -1,6 +1,6 @@
 const User = require('../../db/models/User')
 
 exports.normalizeCrushCookies = async (req, res) => {
-  await User.updateMany({}, { count_crush: 1 })
+  await User.updateMany({ last_crush: { $gt: 1 }, count_crush: { $lte: 0 } }, { count_crush: 1 })
   res.send({ method: 'GET', status: 'ok', api: '/helpers/normalize-crush-cookies' })
 }
